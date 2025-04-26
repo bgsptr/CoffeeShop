d4e8a055026910108d14cf73375cab4d1d207598
bgsptr <agunggusti95@gmail.com>
2025-04-26 19:24:58 +0800

    change http only cookie to https

--- src/interface/controllers/users/user.controller.ts
+++ src/interface/controllers/users/user.controller.ts
@@ -61,19 +61,19 @@ export class UserController {
     try {
       const token = await this.login.execute(loginDto);
 
-      // res.cookie('token', token, {
-      //   httpOnly: true,
-      //   secure: false,
-      //   sameSite: 'lax',
-      // });
-
       res.cookie('token', token, {
         httpOnly: true,
-        secure: true,
-        sameSite: 'none',
-        domain: 'coffeeshop-web-799300494910.asia-southeast2.run.app',
-        path: '/',
+        secure: false,
+        sameSite: 'lax',
       });
+
+      // res.cookie('token', token, {
+      //   httpOnly: true,
+      //   secure: true,
+      //   sameSite: 'none',
+      //   domain: 'coffeeshop-web-799300494910.asia-southeast2.run.app',
+      //   path: '/',
+      // });
 
       const response = new ApiResponse(
         res,
@@ -151,19 +151,19 @@ export class UserController {
   ) {
     const token = await this.oAuth2Google.saveOAuthToJwtAndRedirect(code);
 
-    // res.cookie('token', token, {
-    //   httpOnly: true,
-    //   secure: false,
-    //   sameSite: 'lax',
-    // });
-
     res.cookie('token', token, {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      domain: 'coffeeshop-web-799300494910.asia-southeast2.run.app',
-      path: '/',
+      secure: false,
+      sameSite: 'lax',
     });
+
+    // res.cookie('token', token, {
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: 'none',
+    //   domain: 'coffeeshop-web-799300494910.asia-southeast2.run.app',
+    //   path: '/',
+    // });
 
     // res.redirect('http://localhost:5173/menu');
     res.redirect(`https://coffeeshop-web-799300494910.asia-southeast2.run.app/menu`);
