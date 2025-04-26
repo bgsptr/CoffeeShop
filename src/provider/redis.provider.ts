7ce645899cf2b80c0bd0c21b2f689c1b3e546cb9
bgsptr <agunggusti95@gmail.com>
2025-04-27 00:38:15 +0800

    add localhost cors

--- src/provider/redis.provider.ts
+++ src/provider/redis.provider.ts
@@ -1,16 +1,22 @@
-import { Provider } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import Redis from "ioredis";
+import { Provider } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 
 export const redisProvider: Provider = {
-    provide: 'REDIS_CLIENT',
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService): Redis => {
-        return new Redis({
-            host: configService.get("REDIS_HOST"),
-            port: 6379,
-            username: configService.get("REDIS_USERNAME"),
-            password: configService.get("REDIS_PASSWORD")
-        });
-    }
-}
+  provide: 'REDIS_CLIENT',
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): Redis => {
+    return new Redis({
+      // host: configService.get("REDIS_HOST"),
+      // port: 6379,
+      // username: configService.get("REDIS_USERNAME"),
+      // password: configService.get("REDIS_PASSWORD")
+
+      host: '10.0.0.3',
+      port: 6379,
+      username: 'default',
+      password:
+        '8P8PCA51gwMRdheVkIrCSs41AexYrNi6dHUPoQuxqQBz2xyM5vI7whcbmjo1Pk5GM163QR88Iet7fEr5',
+    });
+  },
+};
